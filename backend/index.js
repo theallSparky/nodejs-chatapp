01ab95298e427fd60145b2fba0726e08dca4bc2d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-18 21:13:59 -0600

    Backend api route to create/read user(s data)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 app.use(express.json())
@@ -7,14 +8,16 @@ app.use(cors({ origin: true }))
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body
-
-try {
-
-} catch (e) {
-
-}
-cbc56182-5688-4e37-8116-e4f563e420ac
-    return res.json({ username: username, secret: "sha256..." })
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "PRIVATE-KEY": "cbc56182-5688-4e37-8116-e4f563e420ac" } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
 })
 
-app.listen(3001)
+app.listen(3001) 
