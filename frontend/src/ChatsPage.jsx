bdee24ed53ee395595775eb9887752c0972d105f
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-19 16:03:54 -0600

    PrettyChatWindow installed, cleaned up files

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,15 +1,14 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
-export const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    'b1be297b-a652-4f76-8e57-a3f7b841be34',
-    props.user.username,
-    props.user.secret
-  )
+const ChatsPage = (props) => {
   return (
     <div style={{ height: '100vh' }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+      <PrettyChatWindow
+        projectId='b1be297b-a652-4f76-8e57-a3f7b841be34'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100%' }}
+      />
     </div>
   )
 }
